e789b42decc1c55274fd84e16d0d54787b56f240
Alen Damadzic <166737547+alendamadzic@users.noreply.github.com>
2026-01-06 17:58:08 +0000

    Prettier dark mode switch

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -1,40 +1,35 @@
 "use client";
 
+import { Switch } from "@/components/ui/switch";
+import { cn } from "@/lib/utils";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import * as React from "react";
-
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="inline-flex items-center gap-2">
+      <span
+        className={cn(
+          "text-left text-sm font-medium",
+          theme === "dark" && "text-muted-foreground/70",
+        )}
+      >
+        <Sun className="size-4" aria-hidden="true" />
+      </span>
+      <Switch
+        checked={theme === "dark"}
+        onCheckedChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+      />
+      <span
+        className={cn(
+          "text-right text-sm font-medium",
+          theme === "light" && "text-muted-foreground/70",
+        )}
+      >
+        <Moon className="size-4" aria-hidden="true" />
+      </span>
+    </div>
   );
 }
